20e5c294a4e085206b5d5f8631b64bd19dd5badc
ᴜɴx Assᴏᴄɪᴀᴛɪᴏɴ <152268815+UnxAssociation@users.noreply.github.com>
2024-03-31 00:13:37 -0700

    Update bot.py

--- UnxMusic/core/bot.py
+++ UnxMusic/core/bot.py
@@ -6,11 +6,11 @@ import config
 from ..logging import LOGGER
 
 
-class Unix(Client):
+class Unx(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="UnixMusic",
+            name="UnxMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
